0269f304cb11c9de3f544fc198a1570258d9452c
minjyo <minjyo8823@gmail.com>
2021-01-25 08:27:28 +0900

    백준 G3 최소비용 구하기 2

---
+++ 다익스트라/11779.py
@@ -0,0 +1,42 @@
+import sys
+
+N = int(sys.stdin.readline())
+M = int(sys.stdin.readline())
+
+adL = [[] for y in range(N+1)]
+for i in range(M):
+    a, b, c = map(int, sys.stdin.readline().split())
+    adL[a].append((b, c)) # to, cost
+
+start, end = map(int, sys.stdin.readline().split())
+
+maxDist = 100000000 # 100000(max dist) * 1000(max nodes)
+dist = [maxDist for i in range(N+1)]
+check = [False for i in range(N+1)]
+prev = [0 for i in range(N+1)]
+
+dist[start] = 0
+prev[start] = -1
+for i in range(N-1):
+    minDist = maxDist+1
+    x = -1
+    for j in range(1, N+1):
+        if check[j] == False and minDist > dist[j]:
+            minDist = dist[j]
+            x = j
+
+    check[x] = True
+    for k in adL[x]:
+        if dist[k[0]] > dist[x] + k[1]:
+            dist[k[0]] = dist[x] + k[1]
+            prev[k[0]] = x
+            
+print(dist[end])
+stack = []
+x = end
+while x != -1:
+    stack.append(x)
+    x = prev[x]
+print(len(stack))
+for i in range(len(stack)-1, -1, -1):
+    print(stack[i], end=' ')
